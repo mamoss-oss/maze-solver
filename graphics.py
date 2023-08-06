60d0fb4561e8f3ab857639dc0cca0e4f9ab7acdb
mmossler <mario.mossler+github@gmail.com>
2023-08-06 19:06:24 +0200

    Stage 3 - Added cell class

--- graphics.py
+++ graphics.py
@@ -1,30 +1,12 @@
 from tkinter import Tk, BOTH, Canvas
 
 
-class Point:
-    def __init__(self, x: int, y: int) -> None:
-        self.x = x
-        self.y = y
-
-
-class Line:
-    def __init__(self, p1: Point, p2: Point) -> None:
-        self.x1 = p1.x
-        self.y1 = p1.y
-        self.x2 = p2.x
-        self.y2 = p2.y
-
-    def draw(self, canvas: Canvas, fill_color: str):
-        canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2)
-        canvas.pack()
-
-
 class Window:
     def __init__(self, width: int, heigth: int) -> None:
         self.__root = Tk()  # Root widget
         self.__root.title("Maze Solver")
         self.__canvas = Canvas(self.__root, width=width, height=heigth, bg="white")
-        self.__canvas.pack()
+        self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -41,5 +23,65 @@ class Window:
     def close(self) -> None:
         self.__running = False
 
-    def draw_line(self, line: Line, fill_color: str) -> None:
+    def draw_line(self, line, fill_color: str = "black") -> None:
         line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1: Point, p2: Point) -> None:
+        self.x1 = p1.x
+        self.y1 = p1.y
+        self.x2 = p2.x
+        self.y2 = p2.y
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2)
+        canvas.pack(fill=BOTH, expand=1)
+
+
+class Cell:
+    def __init__(self, win: Window, p1: Point, p2: Point) -> None:
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = p1.x
+        self._x2 = p2.x
+        self._y1 = p1.y
+        self._y2 = p2.y
+        self._win = win
+
+    def draw(self):
+        """
+        Square:
+        Top Left:       x1, y1
+        Top Right:      x2, y1
+        Bottom Left:    x1, y2
+        Bottom Right:   x2, y2
+        """
+        if self.has_left_wall:
+            p1 = Point(self._x1, self._y1)
+            p2 = Point(self._x1, self._y2)
+            left_wall = Line(p1, p2)
+            self._win.draw_line(left_wall)
+        if self.has_right_wall:
+            p1 = Point(self._x2, self._y1)
+            p2 = Point(self._x2, self._y2)
+            right_wall = Line(p1, p2)
+            self._win.draw_line(right_wall)
+        if self.has_top_wall:
+            p1 = Point(self._x1, self._y1)
+            p2 = Point(self._x2, self._y1)
+            top_wall = Line(p1, p2)
+            self._win.draw_line(top_wall)
+        if self.has_bottom_wall:
+            p1 = Point(self._x1, self._y2)
+            p2 = Point(self._x2, self._y2)
+            bottom_wall = Line(p1, p2)
+            self._win.draw_line(bottom_wall)
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from graphics import Window, Point, Line
+from graphics import Window, Point, Line, Cell
 
 
 def main():
     win = Window(800, 600)
-    p1 = Point(0, 0)
-    p2 = Point(800, 600)
-    l1 = Line(p1, p2)
-    win.draw_line(l1, "black")
+
+    # p1 = Point(0, 0)
+    # p2 = Point(800, 600)
+    # l1 = Line(p1, p2)
+    # win.draw_line(l1, "black")
+
+    c1 = Cell(win, Point(50, 50), Point(100, 100))
+    c1.draw()
+    c2 = Cell(win, Point(150, 150), Point(200, 200))
+    c2.has_bottom_wall = False
+    c2.draw()
+    c3 = Cell(win, Point(250, 250), Point(300, 300))
+    c3.has_right_wall = False
+    c3.draw()
+    c4 = Cell(win, Point(350, 350), Point(400, 400))
+    c4.has_top_wall = False
+    c4.draw()
     win.wait_for_close()
 
 
